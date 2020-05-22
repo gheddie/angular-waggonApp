48cab09b3b9461a7078ddf8f4b15c91159203871
Stefan <stefan.schulz.1976@googlemail.com>
2020-05-22 15:31:04 +0200

    made http work better

--- src/app/shared/waggon.service.ts
+++ src/app/shared/waggon.service.ts
@@ -15,19 +15,14 @@ export class WaggonService {
 
   postWaggon() {
     console.log('service now posting...');
-    const body = {movedWaggonNumber: 'Waggon 1', destinationWaggonNumber: 'Waggon 134.429oooooooooo'};
+    const body = {movedWaggonNumber: 'Waggon 17', destinationWaggonNumber: 'Waggon 34'};
     this.client.post('http://localhost:8080/moveWaggons', body).subscribe();
     console.log('service posted...');
   }
 
   getWaggons(): WaggonDto[] {
 
-    // const jsonString = '{trainNumber: \'123booooooooooooner\', waggons: [{waggonNumber: \'W11\'}, {waggonNumber: \'W123456\'}]}';
-    // JSON.parse(jsonString);
-
-    console.log('parsed: ' + JSON.parse('{"name": "Bob", "error": false}'));
-
-    // ---
+    let waggons = new Array();
 
     this.client.get('http://localhost:8080/waggondata')
       .subscribe(data => {
@@ -39,28 +34,17 @@ export class WaggonService {
         console.log('read waggon list length: ' + length);
 
         for (let index = 0; index < length; index++) {
-          // console.log('Block statement execution no.');
           console.log('read waggon ' + index + ': ' + data['waggons'][index]['waggonNumber']);
+          waggons.push(new WaggonDto(data['waggons'][index]['waggonNumber']));
         }
-
-        /*
-        console.log('read waggon 1: ' + data['waggons'][0]['waggonNumber']);
-        console.log('read waggon 2: ' + data['waggons'][1]['waggonNumber']);
-        console.log('read waggon 3: ' + data['waggons'][2]['waggonNumber']);
-        console.log('read waggon 3: ' + data['waggons'][3]['waggonNumber']);
-        */
       }, error => {
         console.log('error: ');
       });
 
-    // return new WaggonDto();
-    let waggons;
-    waggons = [
-      new WaggonDto('123hanke'),
-      new WaggonDto('234banke'),
-      new WaggonDto('345zanke'),
-      new WaggonDto('123hankeooooooooooooooo')
-    ];
+    /*
+    waggons.push(new WaggonDto('123_hanke'));
+    waggons.push(new WaggonDto('234_banke'));
+    */
 
     return waggons;
   }
